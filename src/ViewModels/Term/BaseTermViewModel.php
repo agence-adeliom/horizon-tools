a45e293736eab96786417c6ce9f7aa69be4bf05b
Lucas Vigneron <lucasvigneron@icloud.com>
2024-07-10 22:12:47 +0200

    feat(view-models): add base term view model (#51)
    Co-authored-by: Lucas Vigneron <lucas.vigneron@adeliom.com>

---
+++ src/ViewModels/Term/BaseTermViewModel.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LucasVigneron\SageTools\ViewModels\Term;
+
+class BaseTermViewModel
+{
+	public ?int $id = null;
+	public ?int $term_id = null;
+	public ?string $name = null;
+	public ?string $slug = null;
+	public ?int $parent = null;
+	public ?int $count = null;
+
+	public function __construct(\WP_Term $term)
+	{
+		$this->id = $term->term_id;
+		$this->term_id = $term->term_id;
+		$this->name = $term->name;
+		$this->slug = $term->slug;
+		$this->parent = $term->parent;
+		$this->count = $term->count;
+	}
+
+	public function toStdClass(): \stdClass
+	{
+		$array = get_object_vars($this);
+
+		return json_decode(json_encode($array));
+	}
+}
