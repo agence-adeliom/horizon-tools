fa09feb6d0334afbe918660109a27f271e999fea
EmmAdeliom <emma.louviot@adeliom.com>
2024-08-08 10:29:35 +0200

    feat(services): add GF listing forms

---
+++ src/Services/FormService.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adeliom\HorizonTools\Services;
+
+use GFFormsModel;
+
+use FilesystemIterator;
+
+class FormService
+{
+    public static function getAllForms(): array
+    {
+        $forms = [];
+
+        if (class_exists(('GFFormsModel'))) {
+            $forms = GFFormsModel::get_forms();
+        }
+
+        return $forms;
+    }
+
+    public static function getAllFormChoices(): array
+    {
+        $choices = [];
+
+        foreach (self::getAllForms() as $form) {
+            $choices[$form->id] = $form->title;
+        }
+
+        return $choices;
+    }
+}
