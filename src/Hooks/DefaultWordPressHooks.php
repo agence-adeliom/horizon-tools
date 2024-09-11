568700e8398a9be3e66f696420fc00de8cab8351
Emmadeliom <68732127+Emmadeliom@users.noreply.github.com>
2024-09-11 11:24:26 +0200

    fix(hooks): add exception for SVG files

--- src/Hooks/DefaultWordPressHooks.php
+++ src/Hooks/DefaultWordPressHooks.php
@@ -9,42 +9,67 @@ use Illuminate\Support\Facades\Config;
 
 class DefaultWordPressHooks extends AbstractHook
 {
-	public function init(): void
-	{
-		add_filter('upload_mimes', [$this, 'allowedMimeTypes']);
-		add_filter('wp_handle_upload_prefilter', [$this, 'cleanMedias']);
-	}
+    public function init(): void
+    {
+        $filters = [
+            ['upload_mimes', [$this, 'allowedMimeTypes']],
+            ['wp_handle_upload_prefilter', [$this, 'cleanMedias']],
+            ['image_downsize', [$this, 'svgAttributes'], 10, 2],
+        ];
 
-	public function allowedMimeTypes($mimes): array
-	{
-		if (is_array($mimes)) {
-			if (Config::get('medias.allow.svg')) {
-				$mimes['svg'] = 'image/svg+xml';
-			}
-		}
+        foreach ($filters as $filter) {
+            add_filter(...$filter);
+        }
+    }
 
-		return $mimes;
-	}
+    public function allowedMimeTypes($mimes): array
+    {
+        if (is_array($mimes)) {
+            if (Config::get('medias.allow.svg')) {
+                $mimes['svg'] = 'image/svg+xml';
+            }
+        }
 
-	public function cleanMedias($file): array
-	{
-		switch ($file['type']) {
-			case 'image/svg+xml':
-				if (Config::get('medias.sanitize.svg')) {
-					$sanitizer = new Sanitizer();
-					$cleanSvg = $sanitizer->sanitize(file_get_contents($file['tmp_name']));
+        return $mimes;
+    }
 
-					if ($cleanSvg) {
-						file_put_contents($file['tmp_name'], $cleanSvg);
-					} else {
-						$file['error'] = 'Failed to sanitize SVG';
-					}
-				}
-				break;
-			default:
-				break;
-		}
+    public function cleanMedias($file): array
+    {
+        switch ($file['type']) {
+            case 'image/svg+xml':
+                if (Config::get('medias.sanitize.svg')) {
+                    $sanitizer = new Sanitizer();
+                    $cleanSvg = $sanitizer->sanitize(file_get_contents($file['tmp_name']));
 
-		return $file;
-	}
+                    if ($cleanSvg) {
+                        file_put_contents($file['tmp_name'], $cleanSvg);
+                    } else {
+                        $file['error'] = 'Failed to sanitize SVG';
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        return $file;
+    }
+
+    public function svgAttributes($out, $id)
+    {
+        $image_url = wp_get_attachment_url($id);
+        $file_ext = pathinfo($image_url, PATHINFO_EXTENSION);
+
+        if (is_admin() || 'svg' !== $file_ext) {
+            return false;
+        }
+
+        return [
+            $image_url,
+            null,
+            null,
+            false,
+        ];
+
+    }
 }
