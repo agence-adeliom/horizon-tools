72329ecd7184ce64efce1140037ed540c4593032
Lucas Vigneron <lucas.vigneron@adeliom.com>
2024-10-09 15:16:07 +0200

    fix(layout-fields): remove final + add "FIELD" prefix to constants

--- src/Fields/Layout/LayoutField.php
+++ src/Fields/Layout/LayoutField.php
@@ -12,87 +12,84 @@ use Extended\ACF\ConditionalLogic;
 
 class LayoutField
 {
-    final public const MEDIA_POSITION = "media_position";
-    final public const DARK_MODE = "dark_mode";
+    public const FIELD_MEDIA_POSITION = 'media_position';
+    public const VALUE_MEDIA_POSITION_LEFT = 'left';
+    public const VALUE_MEDIA_POSITION_RIGHT = 'right';
+    public const VALUE_MEDIA_POSITION_BOTTOM = 'bottom';
 
-    final public const MARGIN = "margin";
+    public const FIELD_DARK_MODE = 'dark_mode';
 
-    final public const MARGIN_SIZES = "sizes";
-    final public const MARGIN_TOP_SIZE = "top_size";
-    final public const MARGIN_BOTTOM_SIZE = "bottom_size";
-    final public const MARGIN_TOP_REMOVE = "top_remove";
-    final public const MARGIN_BOTTOM_REMOVE = "bottom_remove";
+    public const FIELD_MARGIN = 'margin';
 
-    final public const MEDIA_RATIO = "media_ratio";
-    final public const HAS_MEDIA_RATIO = "has_ratio";
-    final public const MEDIA_RATIO_VALUE = "ratio";
+    public const FIELD_MARGIN_TOP_SIZE = 'top_size';
+    public const FIELD_MARGIN_BOTTOM_SIZE = 'bottom_size';
+
+    public const FIELD_MEDIA_RATIO = 'media_ratio';
+    public const FIELD_HAS_MEDIA_RATIO = 'has_ratio';
+    public const FIELD_MEDIA_RATIO_VALUE = 'ratio';
 
     public static function darkMode(): TrueFalse
     {
-        return TrueFalse::make(__('Dark mode'), self::DARK_MODE)
+        return TrueFalse::make(__('Dark mode'), self::FIELD_DARK_MODE)
             ->helperText('Activer le fond sombre pour ce bloc')
             ->stylized();
     }
 
-    public static function mediaPosition(array $choices = [
-        'left'   => 'À gauche',
-        'right'  => 'À droite'
-    ]): RadioButton
-    {
-        return RadioButton::make(__('Position du média'), self::MEDIA_POSITION)
+    public static function mediaPosition(
+        array $choices = [
+            self::VALUE_MEDIA_POSITION_LEFT => 'À gauche',
+            self::VALUE_MEDIA_POSITION_RIGHT => 'À droite',
+            self::VALUE_MEDIA_POSITION_BOTTOM => 'En bas',
+        ]
+    ): RadioButton {
+        return RadioButton::make(__('Position du média'), self::FIELD_MEDIA_POSITION)
             ->choices($choices)
-            ->default("left")
+            ->default('left')
             ->required();
     }
 
-    public static function margin(array $fields = [
-        self::MARGIN_TOP_SIZE,
-        self::MARGIN_BOTTOM_SIZE,
-    ]): Group
+    public static function margin(array $fields = [self::FIELD_MARGIN_TOP_SIZE, self::FIELD_MARGIN_BOTTOM_SIZE]): Group
     {
-
         $fieldsGroup = [];
 
-        if (in_array(self::MARGIN_TOP_SIZE, $fields)) {
-            $fieldsGroup[] = Select::make("Taille de la marge supérieure", self::MARGIN_TOP_SIZE)
+        if (in_array(self::FIELD_MARGIN_TOP_SIZE, $fields)) {
+            $fieldsGroup[] = Select::make('Taille de la marge supérieure', self::FIELD_MARGIN_TOP_SIZE)
                 ->choices([
-                    "none" => "Nulle",
-                    "small" => "Petite",
-                    "large" => "Grande"
+                    'none' => 'Nulle',
+                    'small' => 'Petite',
+                    'large' => 'Grande',
                 ])
-                ->default("large")
-                ->helperText("");
+                ->default('large')
+                ->helperText('');
         }
 
-        if (in_array(self::MARGIN_BOTTOM_SIZE, $fields)) {
-            $fieldsGroup[] = Select::make("Taille de la marge inférieure", self::MARGIN_BOTTOM_SIZE)
+        if (in_array(self::FIELD_MARGIN_BOTTOM_SIZE, $fields)) {
+            $fieldsGroup[] = Select::make('Taille de la marge inférieure', self::FIELD_MARGIN_BOTTOM_SIZE)
                 ->choices([
-                    "none" => "Nulle",
-                    "small" => "Petite",
-                    "large" => "Grande"
+                    'none' => 'Nulle',
+                    'small' => 'Petite',
+                    'large' => 'Grande',
                 ])
-                ->default("large")
-                ->helperText("");
+                ->default('large')
+                ->helperText('');
         }
 
-        return Group::make("Marges", self::MARGIN)->fields($fieldsGroup);
+        return Group::make('Marges', self::FIELD_MARGIN)->fields($fieldsGroup);
     }
 
     public static function mediaRatio(): Group
     {
         $fieldsGroup = [
-            TrueFalse::make("Contraindre le ratio du média", self::HAS_MEDIA_RATIO)
-                ->stylized(),
-            RadioButton::make("Ratio", self::MEDIA_RATIO_VALUE)->choices([
-                "auto" => "Automatique",
-                "paysage" => "Paysage",
-                "portrait" => "Portrait"
-            ])->conditionalLogic([
-                ConditionalLogic::where("has_ratio", "==", 1)
-            ]),
+            TrueFalse::make('Contraindre le ratio du média', self::FIELD_HAS_MEDIA_RATIO)->stylized(),
+            RadioButton::make('Ratio', self::FIELD_MEDIA_RATIO_VALUE)
+                ->choices([
+                    'auto' => 'Automatique',
+                    'paysage' => 'Paysage',
+                    'portrait' => 'Portrait',
+                ])
+                ->conditionalLogic([ConditionalLogic::where('has_ratio', '==', 1)]),
         ];
 
-
-        return Group::make("Ratio du média", self::MEDIA_RATIO)->fields($fieldsGroup);
+        return Group::make('Ratio du média', self::FIELD_MEDIA_RATIO)->fields($fieldsGroup);
     }
 }
