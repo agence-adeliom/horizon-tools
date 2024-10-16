f776208a46929b5df030c1bdd292ce30f15f0ea3
Lucas Vigneron <lucas.vigneron@adeliom.com>
2024-10-16 17:11:00 +0200

    feat(menu): remove throw from front

--- src/ViewModels/Menu/MenuViewModel.php
+++ src/ViewModels/Menu/MenuViewModel.php
@@ -6,51 +6,54 @@ namespace Adeliom\HorizonTools\ViewModels\Menu;
 
 class MenuViewModel
 {
-	public int $id;
-	public string $name;
-	public string $slug;
-	public array $items = [];
+    public int $id;
+    public string $name;
+    public string $slug;
+    public array $items = [];
 
-	public function __construct(\WP_Term|string $menu)
-	{
-		if (is_string($menu)) {
+    public function __construct(\WP_Term|string $menu)
+    {
+        if (is_string($menu)) {
+            if (($locations = get_nav_menu_locations()) && isset($locations[$menu])) {
+                $menu = get_term($locations[$menu]);
+            }
 
-			if (($locations = get_nav_menu_locations()) && isset($locations[$menu])) {
-				$menu = get_term($locations[$menu]);
-			}
+            if (is_string($menu)) {
+                if (is_admin()) {
+                    throw new \Exception('Menu not found');
+                }
+            }
+        }
 
-			if (is_string($menu)) {
-				throw new \Exception('Menu not found');
-			}
-		}
+        if (is_object($menu)) {
+            $this->id = $menu->term_id;
+            $this->name = $menu->name;
+            $this->slug = $menu->slug;
 
-		$this->id = $menu->term_id;
-		$this->name = $menu->name;
-		$this->slug = $menu->slug;
+            $this->setItems();
+        }
+    }
 
-		$this->setItems();
-	}
+    private function setItems(?array &$flatItems = null, ?MenuItemViewModel $parent = null): void
+    {
+        if (null === $flatItems) {
+            $flatItems = wp_get_nav_menu_items($this->id);
+        }
 
-	private function setItems(?array &$flatItems = null, ?MenuItemViewModel $parent = null): void
-	{
-		if (null === $flatItems) {
-			$flatItems = wp_get_nav_menu_items($this->id);
-		}
+        $parentId = $parent ? $parent->id : 0;
 
-		$parentId = $parent ? $parent->id : 0;
+        foreach ($flatItems as $flatItem) {
+            if ($flatItem->menu_item_parent == $parentId) {
+                $item = new MenuItemViewModel($flatItem);
 
-		foreach ($flatItems as $flatItem) {
-			if ($flatItem->menu_item_parent == $parentId) {
-				$item = new MenuItemViewModel($flatItem);
+                $this->setItems($flatItems, $item);
 
-				$this->setItems($flatItems, $item);
-
-				if ($parent) {
-					$parent->addItem($item);
-				} else {
-					$this->items[] = $item;
-				}
-			}
-		}
-	}
+                if ($parent) {
+                    $parent->addItem($item);
+                } else {
+                    $this->items[] = $item;
+                }
+            }
+        }
+    }
 }
