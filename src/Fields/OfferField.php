16e7d4467043a77c6c511bd4efff5909e171745e
Lucas Vigneron <lucas.vigneron@adeliom.com>
2024-12-18 15:20:41 +0100

    feat(offer-field): add OfferField class for managing offer banner settings
    Added a new class `OfferField` to handle the offer banner settings.
    This class includes methods to create a group with a toggle for enabling
    the offer, along with fields for an uptitle and WYSIWYG content, both
    conditionally displayed based on the toggle state.

---
+++ src/Fields/OfferField.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adeliom\HorizonTools\Fields;
+
+use Adeliom\HorizonTools\Fields\Text\UptitleField;
+use Adeliom\HorizonTools\Fields\Text\WysiwygField;
+use Extended\ACF\ConditionalLogic;
+use Extended\ACF\Fields\Group;
+use Extended\ACF\Fields\TrueFalse;
+
+class OfferField
+{
+    final public const OFFER = 'offer';
+    final public const OFFER_ENABLE = 'enable';
+
+    public static function make(string $label = "Bandeau d'offre", string|null $name = self::OFFER): Group
+    {
+        return Group::make("Bandeau d'offre", $name)->fields([
+            TrueFalse::make('Activer', self::OFFER_ENABLE)->stylized(),
+            UptitleField::make()
+                ->required()
+                ->conditionalLogic([ConditionalLogic::where(self::OFFER_ENABLE, '==', '1')]),
+            WysiwygField::minimal()
+                ->required()
+                ->conditionalLogic([ConditionalLogic::where(self::OFFER_ENABLE, '==', '1')]),
+        ]);
+    }
+}
