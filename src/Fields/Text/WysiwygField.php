b05cf05335b2b9131df4f325d5821bda0071f704
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-02-25 16:07:38 +0100

    fix(WysiwygField): update class name references to match the correct casing for WYSIWYGEditor

--- src/Fields/Text/WysiwygField.php
+++ src/Fields/Text/WysiwygField.php
@@ -4,40 +4,39 @@ declare(strict_types=1);
 
 namespace Adeliom\HorizonTools\Fields\Text;
 
-use Extended\ACF\Fields\WysiwygEditor;
+use Extended\ACF\Fields\WYSIWYGEditor;
 
 class WysiwygField
 {
-    final public const WYSIWYG = "wysiwyg";
+    final public const WYSIWYG = 'wysiwyg';
 
     final public const TOOLBAR_DEFAULT = 'default';
     final public const TOOLBAR_SIMPLE = 'simple';
     final public const TOOLBAR_MINIMAL = 'minimal';
 
-
-    public static function make(string $label = "Description", string|null $name = self::WYSIWYG): WysiwygEditor
+    public static function make(string $label = 'Description', string|null $name = self::WYSIWYG): WYSIWYGEditor
     {
-        return WysiwygEditor::make($label, $name);
+        return WYSIWYGEditor::make($label, $name);
     }
 
-    public static function default(string $label = "Description", string|null $name = self::WYSIWYG): WysiwygEditor
+    public static function default(string $label = 'Description', string|null $name = self::WYSIWYG): WYSIWYGEditor
     {
         return self::make($label, $name)
-            ->toolbar( self::TOOLBAR_DEFAULT)
+            ->toolbar(self::TOOLBAR_DEFAULT)
             ->disableMediaUpload(false);
     }
 
-    public static function simple(string $label = "Description", string|null $name = self::WYSIWYG): WysiwygEditor
+    public static function simple(string $label = 'Description', string|null $name = self::WYSIWYG): WYSIWYGEditor
     {
         return self::make($label, $name)
-            ->toolbar( self::TOOLBAR_SIMPLE)
+            ->toolbar(self::TOOLBAR_SIMPLE)
             ->disableMediaUpload(false);
     }
 
-    public static function minimal(string $label = "Description", string|null $name = self::WYSIWYG): WysiwygEditor
+    public static function minimal(string $label = 'Description', string|null $name = self::WYSIWYG): WYSIWYGEditor
     {
         return self::make($label, $name)
-            ->toolbar( self::TOOLBAR_MINIMAL)
+            ->toolbar(self::TOOLBAR_MINIMAL)
             ->disableMediaUpload(false);
     }
 }
