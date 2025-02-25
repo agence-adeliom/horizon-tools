3cc0d7fcba64b1d12522495fc89527054f430155
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-02-25 14:55:25 +0100

    refactor(ImageField): standardize string quotes and improve method formatting

--- src/Fields/Medias/ImageField.php
+++ src/Fields/Medias/ImageField.php
@@ -8,24 +8,22 @@ use Extended\ACF\Fields\Image;
 
 class ImageField extends Image
 {
-	final public const IMAGE = "image";
+    final public const IMAGE = 'image';
 
-	public static function make(string $label = "Image", ?string $name = self::IMAGE): static
-	{
-		return parent::make($label, $name)
-			->library("all")
-			->format('array');
-	}
+    public static function make(string $label = 'Image', ?string $name = self::IMAGE): static
+    {
+        return parent::make($label, $name)->library('all')->format('array');
+    }
 
-	public function ratio(int $width = null, int $height = null): static
-	{
-		if (null !== $width && null !== $height) {
-			$this->settings['instructions'] = "Ratio recommandé : " . $width . "x" . $height . "px";
-		} elseif (null !== $width) {
-			$this->settings['instructions'] = "Largeur recommandée : " . $width . "px";
-		} elseif (null !== $height) {
-			$this->settings['instructions'] = "Hauteur recommandée : " . $height . "px";
-		}
-		return $this;
-	}
+    public function ratio(?int $width = null, ?int $height = null): static
+    {
+        if (null !== $width && null !== $height) {
+            $this->settings['instructions'] = 'Ratio recommandé : ' . $width . 'x' . $height . 'px';
+        } elseif (null !== $width) {
+            $this->settings['instructions'] = 'Largeur recommandée : ' . $width . 'px';
+        } elseif (null !== $height) {
+            $this->settings['instructions'] = 'Hauteur recommandée : ' . $height . 'px';
+        }
+        return $this;
+    }
 }
