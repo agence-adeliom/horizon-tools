5561c4dc522e8ff71129cfb5636e576f9429d207
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-04-01 09:45:35 +0200

    feat(SeoService): add method to retrieve the current URL

--- src/Services/SeoService.php
+++ src/Services/SeoService.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace Adeliom\HorizonTools\Services;
 
+use Illuminate\Support\Facades\Request;
+
 class SeoService
 {
-	public static function isRankMathActive(): bool
-	{
-		if (function_exists('is_plugin_active')) {
-			return is_plugin_active('seo-by-rank-math/rank-math.php');
-		}
+    public static function isRankMathActive(): bool
+    {
+        if (function_exists('is_plugin_active')) {
+            return is_plugin_active('seo-by-rank-math/rank-math.php');
+        }
 
-		return false;
-	}
+        return false;
+    }
+
+    public static function getCurrentUrl(): string
+    {
+        return Request::fullUrl();
+    }
 }
