820ff83376d1ce52d26b4777f3f111fe15863c63
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-04-17 15:14:43 +0200

    feat(PostHooks): fix `leaveName` parameter handling in `post_link` filter
    This commit corrects an error where the `leaveName` parameter was
    not taken into account when using the `post_link` filter.

--- src/Hooks/PostHooks.php
+++ src/Hooks/PostHooks.php
@@ -8,34 +8,34 @@ use Illuminate\Support\Facades\Config;
 
 class PostHooks extends AbstractHook
 {
-	private ?string $prefix = null;
+    private ?string $prefix = null;
 
-	public function init(): void
-	{
-		if (Config::get('posts.prefix')) {
-			$this->prefix = Config::get('posts.prefix');
-			add_action('generate_rewrite_rules', [$this, 'add_rewrite_rules']);
-			add_filter('post_link', [$this, 'change_blog_links'], 1, 3);
-		}
-	}
+    public function init(): void
+    {
+        if (Config::get('posts.prefix')) {
+            $this->prefix = Config::get('posts.prefix');
+            add_action('generate_rewrite_rules', [$this, 'add_rewrite_rules']);
+            add_filter('post_link', [$this, 'change_blog_links'], 1, 3);
+        }
+    }
 
-	public function add_rewrite_rules($wp_rewrite)
-	{
-		$new_rules = array(
-			$this->prefix . '/(.+?)/?$' => 'index.php?post_type=post&name=' . $wp_rewrite->preg_index(1),
-		);
+    public function add_rewrite_rules($wp_rewrite)
+    {
+        $new_rules = [
+            $this->prefix . '/(.+?)/?$' => 'index.php?post_type=post&name=' . $wp_rewrite->preg_index(1),
+        ];
 
-		$wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
-	}
+        $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+    }
 
-	public function change_blog_links($post_link, $id = 0)
-	{
-		$post = get_post($id);
+    public function change_blog_links($post_link, $id = 0, bool $leaveName = false)
+    {
+        $post = get_post($id);
 
-		if (is_object($post) && $post->post_type == 'post') {
-			return home_url('/' . $this->prefix . '/' . $post->post_name . '/');
-		}
+        if (is_object($post) && $post->post_type == 'post') {
+            return home_url(sprintf('/%s/%s/', $this->prefix, $leaveName ? '%postname%' : $post->post_name));
+        }
 
-		return $post_link;
-	}
+        return $post_link;
+    }
 }
