712bbe099d48328f2afa2379b2e97d77f6ec98aa
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-05-20 13:04:06 +0200

    feat(BaseTermViewModel): add description property to model

--- src/ViewModels/Term/BaseTermViewModel.php
+++ src/ViewModels/Term/BaseTermViewModel.php
@@ -6,27 +6,29 @@ namespace Adeliom\HorizonTools\ViewModels\Term;
 
 class BaseTermViewModel
 {
-	public ?int $id = null;
-	public ?int $term_id = null;
-	public ?string $name = null;
-	public ?string $slug = null;
-	public ?int $parent = null;
-	public ?int $count = null;
+    public ?int $id = null;
+    public ?int $term_id = null;
+    public ?string $name = null;
+    public ?string $slug = null;
+    public ?string $description = null;
+    public ?int $parent = null;
+    public ?int $count = null;
 
-	public function __construct(\WP_Term $term)
-	{
-		$this->id = $term->term_id;
-		$this->term_id = $term->term_id;
-		$this->name = $term->name;
-		$this->slug = $term->slug;
-		$this->parent = $term->parent;
-		$this->count = $term->count;
-	}
+    public function __construct(\WP_Term $term)
+    {
+        $this->id = $term->term_id;
+        $this->term_id = $term->term_id;
+        $this->name = $term->name;
+        $this->slug = $term->slug;
+        $this->description = $term->description;
+        $this->parent = $term->parent;
+        $this->count = $term->count;
+    }
 
-	public function toStdClass(): \stdClass
-	{
-		$array = get_object_vars($this);
+    public function toStdClass(): \stdClass
+    {
+        $array = get_object_vars($this);
 
-		return json_decode(json_encode($array));
-	}
+        return json_decode(json_encode($array));
+    }
 }
