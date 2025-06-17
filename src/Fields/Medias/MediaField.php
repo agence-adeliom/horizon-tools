688669e3cef26c1d2336f9df066408d430743f7b
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-06-17 16:54:19 +0200

    feat(MediaField): refactor string constants and improve code readability

--- src/Fields/Medias/MediaField.php
+++ src/Fields/Medias/MediaField.php
@@ -11,98 +11,83 @@ use Extended\ACF\Fields\Select;
 
 class MediaField
 {
-    final public const HAS_IMAGE = "image";
-    final public const HAS_VIDEO = "video";
-    final public const IMAGE_POSITION = "imagePosition";
-    final public const IMAGE_POSITION_TOP = "top";
-    final public const IMAGE_POSITION_CENTER = "center";
-    final public const IMAGE_POSITION_BOTTOM = "bottom";
+    final public const HAS_IMAGE = 'image';
+    final public const HAS_VIDEO = 'video';
+    final public const IMAGE_POSITION = 'imagePosition';
+    final public const IMAGE_POSITION_TOP = 'top';
+    final public const IMAGE_POSITION_CENTER = 'center';
+    final public const IMAGE_POSITION_BOTTOM = 'bottom';
 
-    final public const MEDIA = "media";
-    final public const TYPE = "type";
+    final public const MEDIA = 'media';
+    final public const TYPE = 'type';
 
-    public static function make(string $instructions = "", array $includes = [
-        self::HAS_IMAGE,
-        self::HAS_VIDEO,
-    ]): Group
+    public static function make(string $instructions = '', array $includes = [self::HAS_IMAGE, self::HAS_VIDEO]): Group
     {
-
         $choices = [];
 
         $hasImage = in_array(self::HAS_IMAGE, $includes, true);
         $hasVideo = in_array(self::HAS_VIDEO, $includes, true);
 
         if ($hasImage) {
-            $choices[self::HAS_IMAGE] = "Image";
+            $choices[self::HAS_IMAGE] = 'Image';
         }
 
         if ($hasVideo) {
-            $choices[self::HAS_VIDEO] = "Vidéo";
+            $choices[self::HAS_VIDEO] = 'Vidéo';
         }
 
         $fields = [
-            Select::make("Type", self::TYPE)
+            Select::make('Type', self::TYPE)
                 ->choices($choices)
-                ->helperText("Choisir le type de média")
+                ->helperText('Choisir le type de média')
                 ->stylized()
                 ->required(),
         ];
 
         if ($hasImage) {
-            $imageField = ImageField::make()
-                ->conditionalLogic([
-                    ConditionalLogic::where(self::TYPE, "==", self::HAS_IMAGE),
-                ]);
+            $imageField = ImageField::make()->conditionalLogic([ConditionalLogic::where(self::TYPE, '==', self::HAS_IMAGE)]);
 
             $fields[] = $imageField;
         }
 
         if ($hasVideo) {
-            $videoField = VideoField::make()->conditionalLogic([
-                ConditionalLogic::where(self::TYPE, "==", self::HAS_VIDEO),
-            ]);
+            $videoField = VideoField::make()->conditionalLogic([ConditionalLogic::where(self::TYPE, '==', self::HAS_VIDEO)]);
 
             $fields[] = $videoField;
         }
 
-        return Group::make("Média", self::MEDIA)
+        return Group::make('Média', self::MEDIA)
             ->helperText($instructions)
             ->fields($fields);
     }
 
-    public static function image(string $instructions = "", bool $isImagePosition = false): Group
+    public static function image(string $instructions = '', bool $isImagePosition = false): Group
     {
-
-        return Group::make("Média", self::MEDIA)
+        return Group::make('Média', self::MEDIA)
             ->helperText($instructions)
-            ->fields([
-                $isImagePosition ? self::imagePosition() : "",
-                ImageField::make()
-                    ->conditionalLogic([
-                        ConditionalLogic::where(self::TYPE, "==", self::HAS_IMAGE),
-                    ]),
-            ]);
+            ->fields(
+                array_filter([
+                    $isImagePosition ? self::imagePosition() : null,
+                    ImageField::make()->conditionalLogic([ConditionalLogic::where(self::TYPE, '==', self::HAS_IMAGE)]),
+                ])
+            );
     }
 
-    public static function video(string $instructions = ""): Group
+    public static function video(string $instructions = ''): Group
     {
-        return Group::make("Média", self::MEDIA)
+        return Group::make('Média', self::MEDIA)
             ->helperText($instructions)
-            ->fields([
-                VideoField::make()->conditionalLogic([
-                    ConditionalLogic::where(self::TYPE, "==", self::HAS_VIDEO),
-                ]),
-            ]);
+            ->fields([VideoField::make()->conditionalLogic([ConditionalLogic::where(self::TYPE, '==', self::HAS_VIDEO)])]);
     }
 
     public static function imagePosition()
     {
         return RadioButton::make("Position de l'image", self::IMAGE_POSITION)
             ->choices([
-                self::IMAGE_POSITION_TOP    => 'Ferrée en haut',
+                self::IMAGE_POSITION_TOP => 'Ferrée en haut',
                 self::IMAGE_POSITION_CENTER => 'Centrée',
                 self::IMAGE_POSITION_BOTTOM => 'Ferrée en bas',
             ])
-            ->default(  self::IMAGE_POSITION_CENTER);
+            ->default(self::IMAGE_POSITION_CENTER);
     }
 }
