0641a0eb4a2f4ddf150b45532a12f9891e8070bc
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-07-10 16:23:25 +0200

    feat(DefaultWordPressHooks): enhance admin menu and bar icon handling with dynamic SVG support

--- resources/scripts/back-office/global.js
+++ resources/scripts/back-office/global.js
@@ -1,31 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const adminMenu = document.querySelector('#adminmenu');
-
+const handleAdminMenu = adminMenu => {
     if (adminMenu) {
         const elementsWithSvgIcon = Array.from(adminMenu.querySelectorAll('.wp-menu-image.svg'));
 
         elementsWithSvgIcon.forEach(eltWithSvgIcon => {
             if (eltWithSvgIcon.style.backgroundImage) {
                 if (eltWithSvgIcon.style.backgroundImage.startsWith('url("')) {
-                    const icon = document.createElement('i');
-
-                    icon.style.maskImage = eltWithSvgIcon.style.backgroundImage;
-                    icon.style.webkitMaskImage = eltWithSvgIcon.style.backgroundImage;
-                    icon.style.maskRepeat = 'no-repeat';
-                    icon.style.maskPosition = 'center';
-                    icon.style.maskSize = 'contain';
-                    icon.style.width = '20px';
-                    icon.style.height = '20px';
-                    icon.style.display = 'inline-block';
-
-                    eltWithSvgIcon.style.backgroundImage = 'none';
-                    eltWithSvgIcon.style.display = 'flex';
-                    eltWithSvgIcon.style.alignItems = 'center';
-                    eltWithSvgIcon.style.justifyContent = 'center';
-
-                    eltWithSvgIcon.appendChild(icon);
+                    createIconElement(eltWithSvgIcon, 20);
                 }
             }
         });
     }
+};
+
+const handleAdminBar = adminBar => {
+    if (adminBar) {
+        const elementsWithSvgIcon = Array.from(document.querySelectorAll('.ab-item.svg'));
+
+        if (elementsWithSvgIcon) {
+            elementsWithSvgIcon.forEach(eltWithSvgIcon => {
+                createIconElement(eltWithSvgIcon, 18, 'left');
+            });
+        }
+    }
+};
+
+const createIconElement = (source, size, justify = 'center', align = 'center') => {
+    const icon = document.createElement('i');
+
+    icon.style.maskImage = source.style.backgroundImage;
+    icon.style.webkitMaskImage = source.style.backgroundImage;
+    icon.style.maskRepeat = 'no-repeat';
+    icon.style.maskPosition = 'center';
+    icon.style.maskSize = 'contain';
+    icon.style.width = `${size}px`;
+    icon.style.height = `${size}px`;
+    icon.style.display = 'inline-block';
+
+    source.style.backgroundImage = 'none';
+    source.style.display = 'flex';
+    source.style.alignItems = align;
+    source.style.justifyContent = justify;
+
+    source.appendChild(icon);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const adminMenu = document.querySelector('#adminmenu');
+    const adminBar = document.querySelector('#wpadminbar');
+
+    handleAdminMenu(adminMenu);
+    handleAdminBar(adminBar);
 });
