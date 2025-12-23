a0db0001e7b861a132c5245549acb41123a9ab7b
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-12-23 11:33:10 +0100

    feat(acf-service): enhance ACF field management
    - add methods for secure updating of metadata
    - implement functionality to manage repeater fields and their keys
    - refactor getChoices method for improved readability and performance

--- src/Services/AcfService.php
+++ src/Services/AcfService.php
@@ -4,64 +4,139 @@ declare(strict_types=1);
 
 namespace Adeliom\HorizonTools\Services;
 
+use Adeliom\HorizonTools\Services\AcfService as HorizonToolsAcfService;
+use App\Taxonomies\RealEstate\Data\RealEstateAgentTaxonomy;
 use Extended\ACF\Fields\Group;
 use Extended\ACF\Fields\Repeater;
+use Extended\ACF\Key;
 
 class AcfService
 {
-	public static function getChoices(iterable $fields, string $fullMetaKey, ?string &$buildKey = '', bool $first = true)
-	{
-		foreach ($fields as $field) {
-			if ($first) {
-				$buildKey = '';
-			}
-			switch (true) {
-				case $field instanceof Group:
-				case $field instanceof Repeater:
-				foreach ((array)$field as $key => $value) {
-					if (str_contains($key, 'settings')) {
+    public static function getFieldKey(string $fieldName, string $entityTypeSlug, bool $isTaxonomy = false): string
+    {
+        if ($isTaxonomy) {
+            $notHashedKey = sprintf('group_taxonomy_%s_%s', strtolower(str_replace(['-'], ['_'], $entityTypeSlug)), $fieldName);
+        } else {
+            $notHashedKey = sprintf('group_%s_%s', strtolower(str_replace(['-'], ['_'], $entityTypeSlug)), $fieldName);
+        }
 
-						if (isset($value['sub_fields'])) {
-							if (!empty($buildKey)) {
-								$buildKey .= '_';
-							}
+        return sprintf('field_%s', Key::hash($notHashedKey));
+    }
 
-							$buildKey .= $value['name'];
+    public static function metaSecureUpdate(
+        int $entityId,
+        string $entityTypeSlug,
+        string $metaKey,
+        mixed $metaValue,
+        bool $isTerm = false
+    ): void {
+        if ($isTerm) {
+            update_term_meta(term_id: $entityId, meta_key: $metaKey, meta_value: $metaValue);
+            update_term_meta(
+                term_id: $entityId,
+                meta_key: sprintf('_%s', $metaKey),
+                meta_value: self::getFieldKey(fieldName: $metaKey, entityTypeSlug: $entityTypeSlug, isTaxonomy: true)
+            );
+        } else {
+            update_post_meta(post_id: $entityId, meta_key: $metaKey, meta_value: $metaValue);
+            update_post_meta(
+                post_id: $entityId,
+                meta_key: sprintf('_%s', $metaKey),
+                meta_value: self::getFieldKey(fieldName: $metaKey, entityTypeSlug: $entityTypeSlug)
+            );
+        }
+    }
 
-							if ($results = self::getChoices($value['sub_fields'], $fullMetaKey, $buildKey, false)) {
-									return $results;
-								}
-							}
-						}
-					}
-					break;
-				default:
-					break;
-			}
+    public static function emptyRepeaterField(string $key, int $postId): void
+    {
+        global $wpdb;
 
-			foreach ((array)$field as $key => $value) {
-				if (str_contains($key, 'settings')) {
-					if (isset($value['name'])) {
-						$shouldBe = empty($buildKey) ? $value['name'] : $buildKey . '_' . $value['name'];
+        delete_post_meta(post_id: $postId, meta_key: $key);
+        delete_post_meta(post_id: $postId, meta_key: sprintf('_%s', $key));
 
-						if ($shouldBe === $fullMetaKey) {
-							if (isset($value['choices'])) {
-								return $value['choices'];
-							}
-						}
+        $sqlQuery = <<<SQL
+DELETE FROM {$wpdb->postmeta}
+WHERE post_id = $postId AND (meta_key LIKE '{$key}_%' OR meta_key LIKE '_{$key}_%');
+SQL;
 
-					}
-				}
-			}
-		}
+        $wpdb->query($sqlQuery);
+    }
 
-		return false;
-	}
+    public static function updateRepeaterField(string $key, array $rows, int $postId, string $postTypeSlug): void
+    {
+        // Update the repeater field value (counter of elements)
+        update_post_meta(post_id: $postId, meta_key: $key, meta_value: count($rows));
 
-	public static function getAllOptionPages(): array
-	{
-		$pages = [];
+        // Update the repeater field key
+        update_post_meta(
+            post_id: $postId,
+            meta_key: sprintf('_%s', $key),
+            meta_value: self::getFieldKey(fieldName: $key, entityTypeSlug: $postTypeSlug)
+        );
 
-		return $pages;
-	}
+        foreach ($rows as $index => $row) {
+            foreach ($row as $fieldKey => $fieldValue) {
+                update_post_meta(post_id: $postId, meta_key: sprintf('%s_%d_%s', $key, $index, $fieldKey), meta_value: $fieldValue);
+                update_post_meta(
+                    post_id: $postId,
+                    meta_key: sprintf('_%s_%d_%s', $key, $index, $fieldKey),
+                    meta_value: self::getFieldKey(fieldName: sprintf('%s_%s', $key, $fieldKey), entityTypeSlug: $postTypeSlug)
+                );
+            }
+        }
+    }
+
+    public static function getChoices(iterable $fields, string $fullMetaKey, ?string &$buildKey = '', bool $first = true)
+    {
+        foreach ($fields as $field) {
+            if ($first) {
+                $buildKey = '';
+            }
+            switch (true) {
+                case $field instanceof Group:
+                case $field instanceof Repeater:
+                    foreach ((array) $field as $key => $value) {
+                        if (str_contains($key, 'settings')) {
+                            if (isset($value['sub_fields'])) {
+                                if (!empty($buildKey)) {
+                                    $buildKey .= '_';
+                                }
+
+                                $buildKey .= $value['name'];
+
+                                if ($results = self::getChoices($value['sub_fields'], $fullMetaKey, $buildKey, false)) {
+                                    return $results;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            foreach ((array) $field as $key => $value) {
+                if (str_contains($key, 'settings')) {
+                    if (isset($value['name'])) {
+                        $shouldBe = empty($buildKey) ? $value['name'] : $buildKey . '_' . $value['name'];
+
+                        if ($shouldBe === $fullMetaKey) {
+                            if (isset($value['choices'])) {
+                                return $value['choices'];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static function getAllOptionPages(): array
+    {
+        $pages = [];
+
+        return $pages;
+    }
 }
