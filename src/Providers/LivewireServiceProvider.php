8cb48fff83e9e2eeb049e9ba2a9e89ec891081ec
emmadeliom <emma.louviot@adeliom.com>
2026-01-29 17:02:37 +0100

    fix(livewire): fix Route

--- src/Providers/LivewireServiceProvider.php
+++ src/Providers/LivewireServiceProvider.php
@@ -6,6 +6,8 @@ namespace Adeliom\HorizonTools\Providers;
 
 use Adeliom\HorizonTools\Services\ClassService;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 use Roots\Acorn\Sage\SageServiceProvider;
 use function Roots\add_filters;
 
@@ -15,6 +17,16 @@ class LivewireServiceProvider extends SageServiceProvider
     {
         add_filter('wp_head', [$this, 'addLivewireStyle']);
         add_filter('wp_footer', [$this, 'addLivewireScript']);
+
+        if (class_exists(Livewire::class)) {
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/livewire/update', $handle);
+            });
+
+            Livewire::setScriptRoute(function ($handle) {
+                return Route::get('/livewire/livewire.js', $handle);
+            });
+        }
     }
 
     public function addLivewireStyle(): void
